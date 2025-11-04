cacd98cbfad0cccf3148471d35a74fce413fd53c
Alexander Topalo <topaloalexander@gmail.com>
2025-11-04 02:13:39 +0200

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,11 @@
 import config from '@escral/lint'
+import { includeIgnoreFile } from '@eslint/compat'
+import { fileURLToPath } from 'node:url'
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
-export default config()
+// @todo Move to @escral/lint
+
+export default [
+    includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
+    ...await config(),
+]
